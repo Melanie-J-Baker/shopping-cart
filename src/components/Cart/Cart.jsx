bb9e03388260d510a9f7f97b177011ae7fd4fead
Melanie-J-Baker <baker_mel@hotmail.com>
2023-11-13 16:56:45 +0000

    Add dummy content to shop page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,9 +2,15 @@ import Nav from '../Nav/Nav'
 import './Cart.css'
 
 function Cart() {
-    let itemsInCart = [{name: 'dummyProduct', id: 1}, {name: 'dummyProduct2', id: 2}];
+    let itemsInCart = [{title: 'dummyProduct', id: 1, price: '£15.99', desc: 'dummy description of product', image: '/src/assets/images/dummy.jpg'}, {title: 'dummyProduct2', id: 2, price: '£24.49', desc: 'dummy description of product', image: '/src/assets/images/dummy.jpg'}];
     const itemElements = itemsInCart.map(item =>
-        <li key={item.id} className="product">item.name</li>
+        <li key={item.id} className="cart-product">
+            <div>{item.title}</div>
+            <div><img src={item.image} className="cart-item-image"/></div>
+            <div>{item.desc}</div>
+            <div>{item.price}</div>
+        
+        </li>
     );
     return (
         <>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,10 +2,19 @@ import Nav from '../Nav/Nav'
 import './Shop.css'
 
 function Shop() {
+    let products = [{title: 'dummyProduct', id: 1, price: '£15.99', desc: 'dummy description of product', image: '/src/assets/images/dummy.jpg'}, {title: 'dummyProduct2', id: 2, price: '£24.49', desc: 'dummy description of product', image: '/src/assets/images/dummy.jpg'}];
+    const productElements = products.map(item =>
+        <li key={item.id} className="product">
+            <div>{item.title}</div>
+            <div><img src={item.image} className="product-image"/></div>
+            <div>{item.desc}</div>
+            <div>{item.price}</div>
+        </li>
+    );
     return (
         <>
             <Nav/>
-            <div>Products</div>
+            <div>{productElements}</div>
         </>
     )
 }
