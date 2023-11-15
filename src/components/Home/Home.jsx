cd1134e472f2c5b82b56447cb90245b234fcd5de
Melanie-J-Baker <baker_mel@hotmail.com>
2023-11-15 18:18:54 +0000

    Add images to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,9 +6,14 @@ function Home() {
     <div id="home">
       <Nav />
       <div className="welcome-wrapper">
-        <div className="welcome-text">Welcome to All Tech!</div>
+        <div className="welcome-text">Welcome to All Tech</div>
       </div>
       <p className="home-main-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div className="home-images">
+        <img className="image" src="/src/assets/images/image1.jpg" alt="electronics" />
+        <img className="image" src="/src/assets/images/image2.jpg" alt="electronics" />
+        <img className="image" src="/src/assets/images/image3.jpg" alt="electronics" />
+      </div>
     </div>
   )
 }
