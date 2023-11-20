6fc280d19ffebe21953140547ee8008e5b34b27e
Melanie-J-Baker <baker_mel@hotmail.com>
2023-11-20 15:44:01 +0000

    Import images into Home.jsx. Move 'Added' div to under navigation bar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,9 @@
 import Nav from '../Nav/Nav'
 import Footer from '../Footer/Footer'
 import './Home.css'
+import image1 from '../../assets/images/image1.jpg';
+import image2 from '../../assets/images/image2.jpg';
+import image3 from '../../assets/images/image3.jpg';
 
 function Home() {
   return (
@@ -11,9 +14,9 @@ function Home() {
       </div>
       <p className="home-main-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <div className="home-images">
-        <img className="image" src="/src/assets/images/image1.jpg" alt="electronics" />
-        <img className="image" src="/src/assets/images/image2.jpg" alt="tech" />
-        <img className="image" src="/src/assets/images/image3.jpg" alt="gadgets" />
+        <img className="image" src={image1} alt="electronics" />
+        <img className="image" src={image2} alt="tech" />
+        <img className="image" src={image3} alt="gadgets" />
       </div>
       <Footer />
     </div>
